ed1ce1372273ea133a63147b6bf700054dd803da
haerulmutakin <haerulm.takin@gmail.com>
2021-06-30 11:21:22 +0700

    add app constants

---
+++ src/assets/mock/boardColor.js
@@ -0,0 +1,3 @@
+export const BoardColor = [
+    '#b5bcc2', '#f9d900', '#667684', '#e50000', '#a95255'
+]
---
+++ src/assets/mock/priority.js
@@ -0,0 +1,22 @@
+export const priorityList = [
+    {
+        id: 0,
+        name: 'Urgent',
+        color: '#f44335'
+    },
+    {
+        id: 1,
+        name: 'High',
+        color: '#ffca28'
+    },
+    {
+        id: 2,
+        name: 'Normal',
+        color: '#2296f3'
+    },
+    {
+        id: 3,
+        name: 'Low',
+        color: '#f2f2f5'
+    }
+]
